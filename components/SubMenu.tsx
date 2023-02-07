68893b5907105a695faecb59554deaa4a07ea835
Jean Abayo <jeanabayo3@gmail.com>
2023-02-07 18:39:10 +0200

    fix menu link

--- components/SubMenu.tsx
+++ components/SubMenu.tsx
@@ -9,10 +9,12 @@ type SubMenuProps = {
 
 const SubMenu = ({text, href}: SubMenuProps) => {
   return (
-    <div className="w-full text-center bg-gray-100 cursor-pointer py-1.5 hover:bg-gray-300">
-      <Link href={href}>{text}</Link>
-    </div>
-  )
+    <Link href={href}>
+      <div className="w-full text-center bg-gray-100 cursor-pointer py-1.5 hover:bg-gray-300">
+        <a>{text}</a>
+      </div>
+    </Link>
+  );
 }
 
 export default SubMenu
