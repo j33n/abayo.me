04122f1c5c571d8cd49f5a9208ffb04c89d7b12c
Jean Abayo <jeanabayo3@gmail.com>
2023-03-31 17:18:29 +0200

    responsive skills

--- app/components/sections/skills.tsx
+++ app/components/sections/skills.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 import * as Tabs from "@radix-ui/react-tabs";
+import cuid from "cuid";
 
 import { skills } from "../../data/skills";
 
@@ -25,33 +26,40 @@ const SkillsSection = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col justify-center mb-8 md:container md:mx-auto"
+      className="container flex flex-col justify-center mb-8 md:mx-auto"
     >
       <p className="mb-4 text-xl w-fit">Skills</p>
 
-      <Tabs.Root className="flex flex-col w-9/10" defaultValue="languages">
+      <Tabs.Root
+        className="flex flex-col w-full"
+        orientation="horizontal"
+        defaultValue="languages"
+      >
         <Tabs.List
-          // className="flex border-b shrink-0 border-mauve6"
-          className="flex border-b shrink-0"
+          className="flex flex-col border-b md:flex-row shrink-0"
           aria-label="Skills"
         >
           {skills.map((skill) => (
             <Tabs.Trigger
-              className="px-4 h-[40px] flex-1 flex items-center justify-center text-sm leading-none select-none font-normal hover:text-beige-400 dark:hover:text-beige-300 dark:data-[state=active]:text-beige-300 data-[state=active]:text-beige-400 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] dark:data-[state=active]:shadow-beige-300 data-[state=active]:shadow-beige-400 data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] dark:data-[state=active]:focus:shadow-beige-300 data-[state=active]:focus:shadow-beige-400 outline-none cursor-default"
+              className="px-2 min-h-[40px] max-w-fit md:max-w-xs h-[40px] flex-1 flex items-center justify-center text-sm leading-none select-none font-normal hover:text-beige-400 dark:hover:text-beige-300 dark:data-[state=active]:text-beige-300 data-[state=active]:text-beige-400 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] dark:data-[state=active]:shadow-beige-300 data-[state=active]:shadow-beige-400 data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] dark:data-[state=active]:focus:shadow-beige-300 data-[state=active]:focus:shadow-beige-400 outline-none cursor-default"
               value={skill.name}
+              key={skill.name + cuid()}
             >
-              {skill.title}
+              <p className="truncate md:overflow-clip">{skill.title}</p>
             </Tabs.Trigger>
           ))}
         </Tabs.List>
-
         {skills.map((skill) => (
           <Tabs.Content
-            className="flex flex-wrap gap-4 grow p-5 rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-beige-400 dark:focus:shadow-beige-300 data-[state=inactive]:hidden"
+            className="flex flex-wrap gap-4 grow p-5 rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-beige-400 dark:focus:shadow-beige-300 data-[state=inactive]:hidden min-w-20"
             value={skill.name}
+            key={skill.name + cuid()}
           >
             {skill.talents.map((talent) => (
-              <div className="gap-4 border-[1px] border-gray-300 dark:border-gray-700 rounded-[10px] flex-center p-[1px]">
+              <div
+                className="gap-4 border-[1px] border-gray-300 dark:border-gray-700 rounded-[10px] flex-center p-[1px]"
+                key={talent.name + cuid()}
+              >
                 <SkillBox>
                   {!!talent.icon && (
                     <Image
