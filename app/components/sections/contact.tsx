24198a4247e7cca09137ce53c0b712ca05ecede8
Jean Abayo <jeanabayo3@gmail.com>
2023-04-12 10:55:06 +0200

    add calendly link

--- app/components/sections/contact.tsx
+++ app/components/sections/contact.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { RiMailSendFill } from "react-icons/ri";
+
+import { RiMailSendFill, RiCalendarEventFill } from "react-icons/ri";
 
 const ContactSection = () => {
   return (
@@ -7,13 +8,22 @@ const ContactSection = () => {
       id="contact"
       className="flex flex-col justify-center mb-8 md:container md:mx-auto dark:bg-gray-900"
     >
-      <Link
-        href="mailto:jeanabayo3@gmail.com"
-        className="flex items-center p-2 text-gray-900 rounded bg-beige-300 w-fit"
-      >
-        <RiMailSendFill size={32} />
-        <p className="ml-2">reach out</p>
-      </Link>
+      <div className="flex flex-row first:pr-4">
+        <Link
+          href="https://calendly.com/jeanabayo/15mins"
+          className="flex items-center p-2 mr-4 text-gray-900 rounded bg-beige-300 w-fit"
+        >
+          <RiCalendarEventFill size={32} />
+          <p className="ml-2">Calendly</p>
+        </Link>
+        <Link
+          href="mailto:jeanabayo3@gmail.com"
+          className="flex items-center p-2 text-gray-900 rounded bg-beige-300 w-fit"
+        >
+          <RiMailSendFill size={32} />
+          <p className="ml-2">Mail</p>
+        </Link>
+      </div>
     </section>
   );
 };
